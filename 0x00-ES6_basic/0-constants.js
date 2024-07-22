a6c24ad5ce8122650994d8bc6ed076f3ac126321
ItumelengRandyMalau <itumelengrandy5@gmail.com>
2024-07-22 15:00:10 -0700

    task 0 attempt

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,20 @@
+/* Modify:
+ * function taskFirst to instantiate variables using const
+ * function taskNext to instantiate variables using let
+ */
+
+export function taskFirst() {
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
