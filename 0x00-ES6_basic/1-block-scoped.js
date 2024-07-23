80de7b68bab9b9e3fa3dce132e3fc13b823a9a39
ItumelengRandyMalau <itumelengrandy5@gmail.com>
2024-07-23 06:54:10 -0700

    task1 completed

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,13 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  let task;
+  let task2;
 
   if (trueOrFalse) {
     task = true;
     task2 = false;
   }
+  task = false;
+  task2 = true;
 
   return [task, task2];
 }
