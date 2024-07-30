55703086bf3fd0095329966ecc54c99b2517c5f1
ItumelengRandyMalau <itumelengrandy5@gmail.com>
2024-07-31 01:59:57 +0200

    task5

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,11 @@
+export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const dataView = new DataView(buffer);
+
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+
+  dataView.setInt8(position, value);
+  return dataView;
+}
